f2e57f7f1b6aeb6b0ba6814c76673abcc5f64c8a
yura <cobtahacademy16@gmail.com>
2026-08-29 10:37:25 +0300

    for rime

---
+++ EquipmentProject/Controllers/SubCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using EquipmentProject.Data;
+using EquipmentProject.Models;
+using EquipmentProject.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquipmentProject.Controllers
+{
+    public class SubCategoryController : Controller
+    {
+        private ApplicationDbContext _context;
+        public SubCategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = new SubCategoryViewModel { };
+            return View(model);
+        }
+    }
+}
---
+++ EquipmentProject/Models/ViewModels/SubCategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EquipmentProject.Models
+{
+    public class SubCategoryViewModel
+    {
+        public int Id { get; set; }
+        public int SubcategoryId { get; set; }
+        public string NameSubcategory { get; set; }
+
+        public string ShortDescription { get; set; }
+        public bool IsDeleted { get; set; }
+        public string ImgPath { get; set; }
+        public int Order { get; set; }
+
+        public List<Subcategory> Subcategories { get; set; }
+
+        public Category Categories { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
