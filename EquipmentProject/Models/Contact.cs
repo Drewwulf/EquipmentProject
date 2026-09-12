9e8fa24a0123266b98bc1d4b35eeb1cbd9ccd635
yura <cobtahacademy16@gmail.com>
2026-09-12 11:04:50 +0300

    #EQ-38 def-settings

--- EquipmentProject/Models/Contact.cs
+++ EquipmentProject/Models/Contact.cs
@@ -3,13 +3,12 @@
     public class Contact
     {
         public int Id { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Schedule { get; set; }
-        public int SiteSettingId { get; set; }
-        public bool isdeleted { get; set; }
-
+        public string Address { get; set; } = "Adress";
+        public string PhoneNumber { get; set; } = "+380123456789";
+        public string Email { get; set; } = "Compane@company.com";
+        public string Schedule { get; set; } = "Schedule";
+        public int SiteSettingsId { get; set; } = 1;
+        public bool isdeleted { get; set; } = false;
         public SiteSettings siteSettings = new SiteSettings();
     }
 }
--- EquipmentProject/Models/SiteSettings.cs
+++ EquipmentProject/Models/SiteSettings.cs
@@ -3,13 +3,13 @@
     public class SiteSettings
     {
         public int Id {  get; set; }
-        public string ShopName { get; set; }
-        public string ShopDesc { get; set; }
-        public string HeaderInfo { get; set; }
-        public string? SubHeaderInfo { get; set; }
-        public string SocialFacebook { get; set; }
-        public string SocialInstagram { get; set; }
-        public string SocialTelegram { get; set; }
+        public string ShopName { get; set; } = "shop";
+        public string ShopDesc { get; set; } = "Shop Description";
+        public string HeaderInfo { get; set; } = "Header Info";
+        public string? SubHeaderInfo { get; set; } = "SubHeader Info";
+        public string? SocialFacebook { get; set; } = string.Empty;
+        public string? SocialInstagram { get; set; } = string.Empty;
+        public string? SocialTelegram { get; set; } = string.Empty;
 
         public List<Contact> Contacts { get; set; }= new List<Contact>();
     }
