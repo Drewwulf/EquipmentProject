c506d0aed2f86eb08889ea4c6940cf5cb5533d64
yura <cobtahacademy16@gmail.com>
2026-09-12 12:18:43 +0300

    #EQ-40

--- EquipmentProject/Models/ViewModels/CategoryViewModel.cs
+++ EquipmentProject/Models/ViewModels/CategoryViewModel.cs
@@ -7,7 +7,10 @@
         public string ShortDescription { get; set; }
         public bool IsDeleted { get; set; }
         public string? ImgPath { get; set; }
+        public IFormFile MainImage { get; set; }
         public int Order { get; set; }
         public int Id { get; internal set; }
+
+        public List<Category> Categories { get; set; } = new List<Category>();
     }
 }
